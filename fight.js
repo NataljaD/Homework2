db4ec512c842743ccd6ea4dabf1344201c19f41f
NataljaD <nata737mail@gmail.com>
2017-05-25 00:22:47 +0300

    created function fight, obj fighter1 and fighter2

--- fight.js
+++ fight.js
@@ -41,3 +41,38 @@ class ImprovedFighter extends Fighter {
         super.hit(enemy, 2*point)
     }
 }
+
+let fight = (fighter, improvedFighter, ...point) => {
+    var order=0;
+    if(point.length==0){
+        console.log(`May be ${fighter.name} and ${improvedFighter.name} are friends...`);
+        return;
+    }
+
+    for(let p of point) {
+        if(fighter.health<=0) {
+            console.log(`Won ${improvedFighter.name}!`);
+            return;
+        }
+
+        if(improvedFighter.health<=0) {
+            console.log(`Won ${fighter.name}!`);
+            return;
+        }
+
+        if(order%2)
+            fighter.hit(improvedFighter, p);
+        else
+            improvedFighter.doubleHit(fighter, p);
+
+        order++;
+    }
+    console.log(`${fighter.name} and ${improvedFighter.name} survived`);
+}
+
+
+var fighter1 =new ImprovedFighter('Biba', 4 ,100);
+
+var fighter2 =new Fighter('Boba', 8 ,100);
+
+fight(fighter2, fighter1, 1, 1, 2, 1, 5);
